eac3c27ba20a9f3dcf212253980713df5b6b4aca
ksp1510 <42215711+ksp1510@users.noreply.github.com>
2021-05-26 13:58:49 +0530

    Function reverse created for name.py module.

--- name.py
+++ name.py
@@ -1,1 +1,6 @@
-
+# Following function "reverse" takes two strins as input (first name and last name)
+# and reverse the whole string including the space between first and last name.
+def reverse(first_name: str, last_name: str):
+    name = first_name.capitalize() + " " + last_name.capitalize()
+    rev_name = name[::-1].lower()
+    return f"\nName is: {name}.\nReverse of above name is: {rev_name}."
