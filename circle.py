c676096c47823799100be85434cd06093f176548
ksp1510 <42215711+ksp1510@users.noreply.github.com>
2021-05-26 13:44:09 +0530

    Created circle.py module with area function in it.

---
+++ circle.py
@@ -0,0 +1,7 @@
+
+#Following function "area" takes a float value as
+# radius of the circle and returns the area of that circle.
+def area(radius: float):
+    return (radius**2)*3.14
+
+
---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+Author: Kishankumar Sanjaybhai Patel
+Course: Emerging Technologies
+Student Id: C0805822
+Class Code
+"""
