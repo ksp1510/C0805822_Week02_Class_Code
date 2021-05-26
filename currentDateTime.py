0496a96f3a643ef77d63651b46e0a0efdc002eca
ksp1510 <42215711+ksp1510@users.noreply.github.com>
2021-05-26 14:51:55 +0530

    Comments added to currentDateTime.py module.

--- currentDateTime.py
+++ currentDateTime.py
@@ -1,14 +1,24 @@
 import time
 
-current_time = time.asctime().split(" ")
+# import is used to import time library of python
 
+current_time = time.asctime().split(" ")  # Here current time tuple is accessed using asctime()
+
+
+# and is splitted and stored as list items in current_time.
+
+# Following function returns which day is today from the current_time list
 def currentDay():
-    week = {'Sun':'Sunday','Mon':'Monday','Tue':'Tuesday','Wed':'Wednesday',
-            'Thu':'Thursday','Fri':'Friday','Sat':'Saturday'}
+    week = {'Sun': 'Sunday', 'Mon': 'Monday', 'Tue': 'Tuesday', 'Wed': 'Wednesday',
+            'Thu': 'Thursday', 'Fri': 'Friday', 'Sat': 'Saturday'}
     return f"\nToday is: {week.get(current_time[0])}"
 
+
+# Following function returns which date is today from the current_time list in dd/mmm/yyyy format
 def currentDate():
     return f"\nToday's date is: {current_time[2]}/{current_time[1]}/{current_time[4]}"
 
+
+# Following function returns current system time from the current_time list in hh:mm:ss & 24hrs format
 def currentTime():
     return f"\nCurrent time is: {current_time[3]}"
